606be06411dabb38a89405dd63527f469002c44c
newgenrpa <ibpsrpa@gmail.com>
2022-05-04 12:45:29 +0530

    changes for console

--- packages/selenium-ide/src/neo/containers/Console/index.jsx
+++ packages/selenium-ide/src/neo/containers/Console/index.jsx
@@ -93,17 +93,17 @@ export default class Console extends React.Component {
             <LogList
               output={output}
               scrollTo={this.scroll}
-              id="Log"
+              id="Output"
               role="tabpanel"
             />
           )}
-          {this.state.tab === 'Reference' && (
+          {/*this.state.tab === 'Reference' && (
             <CommandReference
               currentCommand={command}
               id="Reference"
               role="tabpanel"
             />
-          )}
+          )*/}
         </div>
         <div className="customdiv">
               <Bottomproceed
